1c03055d6f555bfad58967317a1c567a7cbd1206
= <=>
2017-04-03 19:08:13 +0500

    sb admin integrated

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,13 @@
+
+/*
+*= require jquery
+*=  require jquery_ujs
+*= require admin_custom
+*= require admin/jquery.min
+*= require admin/bootstrap.min
+*= require admin/metisMenu.min
+*= require admin/jquery.dataTables.min
+*= require admin/dataTables.bootstrap.min
+*= require admin/dataTables.responsive
+*= require admin/sb-admin-2
+*/
