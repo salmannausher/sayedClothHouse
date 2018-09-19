d976595e8e361db6fce49490a25f73ff5b023c6d
Salman Nausher <sallu4548@gmail.com>
2018-09-19 17:32:23 -0400

    date added to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180918213849) do
+ActiveRecord::Schema.define(version: 20180919203246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20180918213849) do
     t.integer  "gazana_num"
     t.string   "order_type"
     t.integer  "discount"
+    t.date     "order_date"
   end
 
   create_table "payments", force: :cascade do |t|
